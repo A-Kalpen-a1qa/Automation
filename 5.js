b029c9a8bb4fa4df178059418081bd7dc8d91bad
A-Kalpen-a1qa <a.kalpen@a1qa.com>
2026-03-05 17:02:46 +0500

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,30 +1,32 @@
 class BankAccount {
+  #balance
   constructor(balance, owner) {
-    this.balance = balance
+    this.#balance = balance
     this.owner = owner
   }
-  set deposit(money){
-    this.balance = this.balance + money
+  deposit(money){
+    this.#balance = this.#balance + money
   }
-  set withdraw(money){
-    if(this.balance - money >= 0){
-    this.balance = this.balance - money
+  withdraw(money){
+    if(this.#balance - money >= 0){
+    this.#balance = this.#balance - money
     } else {
         console.log("You don't have enough money")
     }
   }
-  get getBalance(){
-    return this.balance
+  getBalance(){
+    return this.#balance
   }
 }
 
 let account1 = new BankAccount(0, "Amir")
-console.log(account1.getBalance)
-account1.deposit = 100 
-console.log(account1.getBalance)
-account1.withdraw = 50
-console.log(account1.getBalance)
-account1.withdraw = 50
-console.log(account1.getBalance)
-account1.withdraw = 50
-console.log(account1.getBalance)
+
+console.log(account1.getBalance())
+account1.deposit(100)
+console.log(account1.getBalance())
+account1.withdraw(50)
+console.log(account1.getBalance())
+account1.withdraw(50)
+console.log(account1.getBalance())
+account1.withdraw(50)
+console.log(account1.getBalance())
