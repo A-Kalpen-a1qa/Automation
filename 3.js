66bf4bd933ea6211f6db458bb2eb46fd4296dfc2
A-Kalpen-a1qa <a.kalpen@a1qa.com>
2026-03-05 16:47:52 +0500

    Update 3.js

--- 3.js
+++ 3.js
@@ -3,16 +3,16 @@ let score = Number(prompt("Write your score(0-100): "))
 if (score <= 100 && score >= 90){
     console.log("A");
 }
-else if(score <= 89 && score >= 80){
+else if(score >= 80){
     console.log("B");
 }
-else if(score <= 79 && score >= 70){
+else if(score >= 70){
     console.log("C");
 }
-else if(score <= 69 && score >= 60){
+else if(score >= 60){
     console.log("D");
 }
-else if(score <= 60 && score >= 0){
+else if(score >= 0){
     console.log("F");
 }
 else {
